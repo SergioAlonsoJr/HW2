1bcf1a65c2f881964ad25bf841cbd150833356a8
Sérgio Alonso Jr <SergioAwoke@gmail.com>
2021-08-28 18:02:54 -0300

    Just finished chapter 4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,45 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+    #default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update (movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movies_path (@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,:rating, :release_date)
+  end
+end
